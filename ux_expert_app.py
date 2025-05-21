424f47df64f18661df0781d7480d297dae61324a
rina <rinasosiska@yandex.ru>
2025-05-21 21:35:28 +0500

    aaa

--- ux_expert_app.py
+++ ux_expert_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import pytesseract
 from interface import perform_ocr_analysis, check_text_contrast, analyze_clutter
 
 # ========== Текст рекомендаций ==========
